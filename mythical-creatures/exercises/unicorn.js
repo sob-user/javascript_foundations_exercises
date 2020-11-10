c528b0870f9cf071e46190306af1e2dbdc84955e
sob-user <oulad.b.s@gmail.com>
2020-11-10 12:15:08 +0100

    init project

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,20 @@
 class Unicorn {
   constructor(name, color) {
-    this.name = name;
-    this.color = color;
+    this.name = name
+    this.color = color || 'white'
+  }
+
+  isWhite() {
+    return this.color === 'white' ? true : false
+  }
+
+  says(string) {
+    if(string === 'Wonderful!') {
+      return '**;* Wonderful! *;**'
+    }
+    else if(string === 'Batman has a great smile') {
+      return '**;* Batman has a great smile *;**'
+    }
   }
 }
 
